4de2f7f50151ea8fefebeaef54e3680fe5fd38c8
Rohan1885633 <141541061+Rohan1885633@users.noreply.github.com>
2025-07-25 23:35:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -75,7 +75,7 @@ STAR_PREMIUM_PLANS = {
 # ============================
 # MongoDB Configuration
 # ============================
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Rohankumar188:<iSjF3B7yXAjDKWxK>@cluster0.l59u52s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # MongoDB URI for the database
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Rohankumar188:iSjF3B7yXAjDKWxK@cluster0.l59u52s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")  # MongoDB URI for the database
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0") # Database name (default: cluster)
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'dreamcinezone_files') # Collection name (default: dreamcinezone_files)
 
